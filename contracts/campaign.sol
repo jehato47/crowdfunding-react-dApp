fba48679e51abcb3d856eb4136b1e3eeaf53a026
jehato47 <jehatdeniz@hotmail.com>
2023-04-08 15:53:50 +0300

    add memory keyword to Request struct

--- contracts/campaign.sol
+++ contracts/campaign.sol
@@ -31,7 +31,7 @@ contract Campaign {
 
 
     function createRequest(string description, uint value, address recipient) public restricted {
-        Request newRequest = Request({
+        Request memory newRequest = Request({
             description: description,
             value: value,
             recipient: recipient,
