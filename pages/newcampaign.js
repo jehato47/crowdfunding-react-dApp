8998266a3a0f270aa76517ef4e7bab2db2b23070
jehato47 <jehatdeniz@hotmail.com>
2023-04-12 20:23:23 +0300

    add next js library

---
+++ pages/newcampaign.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NewCampaign() {
+    return (
+        <div>
+        <h1>Welcome to the New Campaign page</h1>
+        </div>
+    );
+}
---
+++ pages/show.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NewCampaign() {
+    return (
+        <div>
+        <h1>Welcome to the show page</h1>
+        </div>
+    );
+}
