b16132dd0718d4d9f1e54049f2e8a85357d2722c
jehato47 <jehatdeniz@hotmail.com>
2023-04-14 19:12:35 +0300

    add button to index.js

--- components/layout.js
+++ components/layout.js
@@ -1,15 +1,23 @@
 import React from "react";
 import Header from "./header";
 import { Container } from "semantic-ui-react";
+import Head from "next/head";
 
 export default function Layout({ children }) {
-    return (
-       <Container style={{marginTop:'10px'}}>
-         <div className="layout">
-            <Header/>
-            <div className="content">{children}</div>
-            <h1>I am a footer</h1>
-        </div>
-       </Container>
-    );
+  return (
+    <Container style={{ marginTop: "10px" }}>
+      <Head>
+        <link
+          async
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
+        />
+      </Head>
+
+      <div className="layout">
+        <Header />
+        <div className="content">{children}</div>
+      </div>
+    </Container>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Card, Button } from "semantic-ui-react";
 import factory from "../ethereum/factory";
+import Layout from "../components/layout";
 
 class CampaignIndex extends Component {
   static async getInitialProps() {
@@ -20,11 +21,20 @@ class CampaignIndex extends Component {
   }
   render() {
     return (
+      <Layout>
         <div>
-          
+        
+          <h3>Open Campaigns</h3>
+          <Button
+            floated="right"
+            content="Create Campaign"
+            icon="add circle"
+            primary
+          />
           {this.renderCampaigns()}
         </div>
-      );
+      </Layout>
+    );
   }
 }
 
