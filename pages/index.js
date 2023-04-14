39f66142109d4c972df55c8937a468bddfbee10d
jehato47 <jehatdeniz@hotmail.com>
2023-04-14 13:03:01 +0300

    build card items and render them inside index.js

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,30 @@
 import React, { Component } from "react";
+import { Card, Button } from "semantic-ui-react";
 import factory from "../ethereum/factory";
-
 
 class CampaignIndex extends Component {
   static async getInitialProps() {
     const campaigns = await factory.methods.getDeployedCampaigns().call();
+
     return { campaigns };
   }
-
-
+  renderCampaigns() {
+    const items = this.props.campaigns.map((address) => {
+      return {
+        header: address,
+        description: <a>View Campaign</a>,
+        fluid: true,
+      };
+    });
+    return <Card.Group items={items} />;
+  }
   render() {
-    return <h3>{this.props.campaigns[0]}</h3>;
+    return (
+        <div>
+          
+          {this.renderCampaigns()}
+        </div>
+      );
   }
 }
 
--- pages/show.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function NewCampaign() {
-    return (
-        <div>
-        <h1>Welcome to the show page</h1>
-        </div>
-    );
-}
