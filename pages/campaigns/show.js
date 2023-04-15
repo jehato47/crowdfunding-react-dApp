9920ed5f461423a3b978743b1ba92f0941a93e68
jehato47 <jehatdeniz@hotmail.com>
2023-04-15 21:52:15 +0300

    add show.js file. This page will include details of the contract

---
+++ pages/campaigns/show.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Layout from "../../components/layout";
+
+class CampaignShow extends Component {
+  render() {
+    return (
+      <Layout>
+        <div>
+          <h3>Campaign Show</h3>
+        </div>
+      </Layout>
+    );
+  }
+}
+
+export default CampaignShow;
